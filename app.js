3a03326651aa5b7983b67c99899297dbd7e49be4
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2021-10-29 18:38:09 +0300

    удаление страниц

--- app.js
+++
@@ -1,9 +0,0 @@
-function openbox(id){
-  display = document.getElementById(id).style.display;
-
-  if(display=='none'){
-     document.getElementById(id).style.display='flex';
-  }else{
-     document.getElementById(id).style.display='none';
-  }
-}
